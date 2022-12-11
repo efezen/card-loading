fa4ba1fdd9a2656fa52d2a7dfc1096d2ba1cd032
Efe Özen <efzenn@hotmail.com>
2022-12-11 21:01:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2000);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://i0.wp.com/blog.rescuetime.com/wp-content/uploads/2017/12/norbert-levajsics-243763.jpg?fit=1000%2C667&ssl=1" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/lego/2.jpg" alt="" />';
+  name.innerHTML = "John Doe";
+  date.innerHTML = "11 Aralık 2022";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
